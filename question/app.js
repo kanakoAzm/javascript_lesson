f13eb35b8aef80282dd129a8a25bc47c36e68151
kanakoAzm <knka0717@gmail.com>
2024-09-13 13:02:48 +0900

    javascript_lessonの問題Q3を回答

--- question/app.js
+++ question/app.js
@@ -10,3 +10,13 @@ console.log('私のニックネームは' + nickname + 'です。年齢は' + ag
 let languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go'];
 
 console.log('私の好きな言語は' + languages[0] + 'です。次は' + languages[3] + 'を勉強してみたいです。');
+
+// Q3
+
+let user = {
+  name: 'John',
+  age: 26,
+  bloodType: 'A',
+  favorite: 'card',
+};
+console.log(user.age);
