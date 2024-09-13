7924bfbef1ed0dee83c6d64bbcb38d860bd01742
kanakoAzm <knka0717@gmail.com>
2024-09-13 13:00:06 +0900

    javascript_lessonの問題Q2を回答

--- question/app.js
+++ question/app.js
@@ -4,3 +4,9 @@ let nickname = 'あずまかなこ';
 let age = 23;
 
 console.log('私のニックネームは' + nickname + 'です。年齢は' + age + '歳です。');
+
+// Q2
+
+let languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go'];
+
+console.log('私の好きな言語は' + languages[0] + 'です。次は' + languages[3] + 'を勉強してみたいです。');
